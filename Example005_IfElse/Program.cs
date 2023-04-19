f8c7e81394c22597064b367c9848e75558ed0b7c
VladimirBushilov <ttkshibo@ya.ru>
2023-04-19 17:02:53 +0300

    example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Add ur name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "vladimir")
+{
+    Console.WriteLine("Herey, this is Vladimir!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
